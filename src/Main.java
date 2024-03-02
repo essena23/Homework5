be168d0b9a53a595a19ed2898cd03f8d313e5ebe
User <essena23@gmail.com>
2024-03-02 09:44:17 -0500

    fixed task 2

--- src/Main.java
+++ src/Main.java
@@ -16,22 +16,20 @@ public class Main {
 
         //task 2
         System.out.println("task 2");
-        char clientOS2 = 2;
+        int clientOS2 = 2;
         int clientDeviceYear = 2014 ;
-        if (clientOS2 ==0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
+        if (clientDeviceYear < 2015 && clientOS2 == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-        if (clientOS2 == 1) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-        } else {
+        if (clientDeviceYear >= 2015 && clientOS2 == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientDeviceYear < 2015 && clientOS2 == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientDeviceYear >= 2015 && clientOS2 == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS2 > 1 || clientOS2 < 0) {
             System.out.println("Такого приложения у нас нет");
         }
 
